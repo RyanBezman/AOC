6f99601d7b8ba0342ab81efb8037e236e7aee794
Ryan Bezman <ryanbezman@icloud.com>
2024-12-04 21:54:49 -0500

    partially done with dfs on grid

--- app/scripts/dayFour.ts
+++ app/scripts/dayFour.ts
@@ -1,5 +1,57 @@
 const fs = require("fs");
 const filePathFour = "../texts/dayFour.txt";
-const fileFour = fs.readFileSync(filePathFour, "utf-8");
+const fileFour = fs
+  .readFileSync(filePathFour, "utf-8")
+  .split("\n")
+  .map((row) => row.split(""));
 
+function countXmas(puzzle) {
+  let count = 0;
+  const rows = puzzle.length;
+  const cols = puzzle[0].length;
+  const word = "XMAS";
+
+  const xAxis = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+  const yAxis = [
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1],
+  ];
+
+  const directions = [...xAxis, ...yAxis];
+
+  function searchGrid(row, col, index) {
+    if (index === word.length) {
+      return true;
+    }
+
+    if (
+      row < 0 ||
+      col < 0 ||
+      row >= rows ||
+      col >= cols ||
+      puzzle[row][col] !== word[index]
+    ) {
+      return false;
+    }
+  }
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (puzzle[row][col] === word[0] && searchGrid(row, col, 0)) {
+        count += 1;
+      }
+    }
+  }
+  return count;
+}
+
+const puzzleCount = countXmas(fileFour);
+console.log(puzzleCount);
 console.log(fileFour);
